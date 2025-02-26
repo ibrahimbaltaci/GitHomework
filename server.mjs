d0f91a237c41e1b1a4fbafacb32e7826a10613b5
ibaltaci <ibaltaci001@gmail.com>
2025-02-26 22:51:44 +0300

    Initial commit

---
+++ server.mjs
@@ -0,0 +1,14 @@
+
+import { createServer } from 'node:http';
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<h1>Common module - Git - Homework</h1></br><h2>Feature</h2>');
+});
+
+// starts a simple http server locally on port 3000
+server.listen(3000, '127.0.0.1', () => {
+  console.log('Listening on 127.0.0.1:3000');
+});
+
+// run with `node server.mjs`
